eb9fb3a6c0f4727c7611df7452b87508deabd3ac
wuzandway <95221876+wuzandway@users.noreply.github.com>
2021-12-06 20:36:21 +0800

    Update HW.py

--- HW.py
+++ HW.py
@@ -1,3 +1,5 @@
+import streamlit as st
+
 car_simulation = {'gas_warning':1, 'speed_limit':100, 'temp_warning':30, '轉速':12000}
 gas = int(st.number_input('油量的資料收集:油箱滿是10格 =>'))
 speed= float(st.number_input('車速的資料收集:限速100 =>'))
